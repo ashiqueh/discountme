0c15dfa63a1cd81138cf3a00724e9bec7ba7e7e8
Ashique Hossain <ashique.hossain@mail.mcgill.ca>
2016-05-16 19:02:44 -0400

    retrieves more relevant posts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request
+import itertools
 
 print("Don't forget to chcp 65001") # this is a reminder to me when i run this program from command line
 
@@ -8,6 +9,7 @@ with open('key.txt', 'r') as f:
 
 term = 'food' #search term from user input
 num_tags = 5 # number of tags to search
+num_pages = 3 # number of pages to search
 
 # key is my secret key, which i'm using for testing purposes
 
@@ -50,17 +52,32 @@ print (tags)
 posts = [] #all posts
 
 for tag in tags:
-	response = urllib.request.urlopen('https://api.instagram.com/v1/tags/'+ tag + '/media/recent?' + '&access_token=' + key)
-	response_string = response.read().decode('utf-8')
-	data = json.loads(response_string)
-	data = data['data'] # array of posts
-	for obj in data:
-		posts.append([obj['caption'],obj['likes'],obj['link']])
+	next_min_id = ""
+	for i in range(0, num_pages):
+		response = urllib.request.urlopen('https://api.instagram.com/v1/tags/'+ tag + '/media/recent?' + '&count=33&next_min_id=' + next_min_id + '&access_token=' + key)
+		#print('https://api.instagram.com/v1/tags/'+ tag + '/media/recent?' + '&count=33&access_token=' + key)
+		response_string = response.read().decode('utf-8')
+		data = json.loads(response_string)
+		#pagination info
+		pagination = data.get('pagination', '')
+		#print (pagination)
+		next_min_id = pagination.get('next_min_id', '') #get the next page id if it exists 
+		data = data['data'] # array of posts
+		for obj in data:
+			posts.append([obj['caption'],obj['likes'],obj['link']])
 
+#posts = list(set(posts)) #remove duplicates
+
+#removes duplicates in posts 
+
+# THIS DOESNT WORK because the count on each post is different, even if the post is the same - remove count and solve the problem. 
+posts=list(posts for posts,_ in itertools.groupby(posts))
 print(posts)
 
 #now i have a list of posts 
 #need to retrieve more posts, check notes for SO link to potential solution 
+# -- above is basically done .. 
+
 #retrieve posts, and validate each to see if it's relevant
 #for now, print out body text of the posts which are relevant
 
